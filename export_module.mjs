e50566606d199e65c123792299f6c0c416d55237
Samson <stmg202022@gmail.com>
2022-08-23 09:50:01 +0545

    nodeJs import and export file using  ES6 moudle

---
+++ export_module.mjs
@@ -0,0 +1,20 @@
+
+//==========simple function export===========
+export function sum(a, b){
+
+    return a + b;
+
+}
+
+//============arrow function export==========
+export const myinfo = () =>{
+    console.log("Hello, I am Samson Tamang.");
+}
+
+//=========default function export===========
+// export default function defaultSimple(){
+//     console.log("Default Simple.");
+//     return 200;
+// }
+
+
---
+++ import_module.mjs
@@ -0,0 +1,15 @@
+
+//===========importing all function at once===========
+// import * as exportfile from "./export_module.mjs";
+
+// console.log(exportfile.sum(2, 5));
+
+
+
+//=====================importing certain function by their name=============
+// import {sum, myinfo} from "./export_module.mjs";
+
+
+// console.log(sum(5, 4));
+// console.log(myinfo());
+
